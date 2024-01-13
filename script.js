ac3a7f7abb2496d7bd3ee86d2d50dd04872b944e
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2024-01-13 10:45:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let factNum = factorial(n);
+	// let intNum = parseIngt(factNum,10);
+
+	  var countZeros = 0;
+      while (factNum % 10 === 0) {
+        countZeros++;
+        factNum = factNum / 10;
+      }
+	return countZeros;
 }
 
 const input = prompt("Enter a number");
 alert(trailingZeros(input));
+
+function factorial(n){
+	if(n==0 || n==1){
+		return 1;
+	}else{
+		return n*factorial(n-1);
+	}
+}
